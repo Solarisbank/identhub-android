a86302bea9e7d2c146ac507f42b7df9fa6a97f6a
Jan Ehrhardt <Jan.Ehrhardt@solarisbank.de>
2021-04-16 12:46:50 +0200

    Add logging of HTTP requests (#40)

--- identhub/src/main/java/de/solarisbank/identhub/di/network/NetworkModule.java
+++ identhub/src/main/java/de/solarisbank/identhub/di/network/NetworkModule.java
@@ -10,6 +10,7 @@ import java.util.concurrent.TimeUnit;
 import de.solarisbank.identhub.data.network.interceptor.DynamicBaseUrlInterceptor;
 import de.solarisbank.identhub.domain.session.SessionUrlRepository;
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.CallAdapter;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -38,10 +39,13 @@ public final class NetworkModule {
     }
 
     public OkHttpClient provideOkHttpClient(final DynamicBaseUrlInterceptor dynamicBaseUrlInterceptor) {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
         return new OkHttpClient.Builder()
                 .addInterceptor(dynamicBaseUrlInterceptor)
                 .writeTimeout(TIMEOUT, TimeUnit.SECONDS)
                 .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
+                .addInterceptor(logging)
                 .build();
     }
 
