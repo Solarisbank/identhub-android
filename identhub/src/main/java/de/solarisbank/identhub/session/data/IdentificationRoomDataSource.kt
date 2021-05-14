38fb1efebef4ec9a49383d31c42780e6ac0d4442
aChubenko <oleksandr.chubenko.extern@solarisbank.de>
2021-05-14 17:43:46 +0300

    fix flow selection (#54)
    Signed-off-by: oleksandr.chubenko <oleksandr.chubenko.extern@solarisbank.de>

--- identhub/src/main/java/de/solarisbank/identhub/session/data/IdentificationRoomDataSource.kt
+++ identhub/src/main/java/de/solarisbank/identhub/session/data/IdentificationRoomDataSource.kt
@@ -2,6 +2,7 @@ package de.solarisbank.identhub.session.data
 
 import de.solarisbank.identhub.data.dao.IdentificationDao
 import de.solarisbank.identhub.data.dto.IdentificationDto
+import de.solarisbank.identhub.data.entity.Identification
 import io.reactivex.Maybe
 
 class IdentificationRoomDataSource(private val identificationDao: IdentificationDao) {
@@ -9,7 +10,15 @@ class IdentificationRoomDataSource(private val identificationDao: Identification
     fun getLastIdentification(): Maybe<IdentificationDto> {
         return identificationDao.getAll()
                 .toMaybe()
-                .map { it.last() }
+                .flatMap { lastIdentification(it) }
                 .map { IdentificationDto(id = it.id, status = it.status, url = it.url, documents = null) }
+    }
+
+    private fun lastIdentification(list: List<Identification>): Maybe<Identification> {
+        return if (list.isNotEmpty()) {
+            Maybe.just(list.last())
+        } else {
+            Maybe.empty()
+        }
     }
 }
