856c9fc843a516c62d3ad70003a75bc774fd0535
Mariam Petrosyan <46495217+maripetrosyan@users.noreply.github.com>
2021-08-19 19:20:24 +0200

    replace document type names with readable strings (#230)
    * replace document type names with readable strings
    
    Signed-off-by: oleksandr.chubenko <oleksandr.chubenko.extern@solarisbank.de>

--- identhub/src/main/java/de/solarisbank/identhub/contract/adapter/DocumentViewHolder.java
+++ identhub/src/main/java/de/solarisbank/identhub/contract/adapter/DocumentViewHolder.java
@@ -1,11 +1,10 @@
 package de.solarisbank.identhub.contract.adapter;
 
+import android.content.Context;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.UnderlineSpan;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,6 +13,7 @@ import de.solarisbank.identhub.R;
 import de.solarisbank.identhub.data.entity.Document;
 
 public class DocumentViewHolder extends RecyclerView.ViewHolder {
+
     private final TextView title;
 
     public DocumentViewHolder(View holderView) {
@@ -22,12 +22,28 @@ public class DocumentViewHolder extends RecyclerView.ViewHolder {
     }
 
     public void bind(Document document) {
-        SpannableString spannableString = SpannableString.valueOf(document.getDocumentType());
+        SpannableString spannableString = SpannableString.valueOf(getDocTypeString(document.getDocumentType()));
         spannableString.setSpan(new UnderlineSpan(), 0, spannableString.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
         title.setText(spannableString);
     }
 
     public void bindAction(View.OnClickListener clickListener) {
         title.setOnClickListener(clickListener);
+    }
+
+    private String getDocTypeString(String documentTypeEnum) {
+        Context context = title.getContext();
+        switch (documentTypeEnum) {
+            case "QES_DOCUMENT":
+                return context.getString(R.string.qes_document);
+            case "LOAN_MANDATE_CONTRACT":
+                return context.getString(R.string.loan_mandate_contract);
+            case "SIGNED_QES_DOCUMENT":
+                return context.getString(R.string.signed_qes_document);
+            case "SIGNED_LOAN_MANDATE_CONTRACT":
+                return context.getString(R.string.signed_loan_mandate_contract);
+            default:
+                return documentTypeEnum;
+        }
     }
 }
