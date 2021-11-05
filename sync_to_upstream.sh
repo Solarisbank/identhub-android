1b704251546912322ed194f8aee8b5cd908a0f2c
Jan Ehrhardt <Jan.Ehrhardt@solarisbank.de>
2021-11-05 13:56:25 +0100

    Add script for syncing to upstream (#271)

---
+++ sync_to_upstream.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+# USAGE: RELEASE_VERSION=< Git tag of the version to publish > ./sync_to_upstream.sh
+
+if [[ ! -z "${RELEASE_VERSION}" ]]; then
+    echo "Sync version ${RELEASE_VERSION} to Solarisbank/identhub-android"
+else
+	echo "Set the RELEASE_VERSION to sync 🙏🏻"
+  exit
+fi
+
+if [[ $(git diff --stat) != '' ]]; then
+  echo 'Commit or stash your local changes before continuing with the sync'
+  exit
+fi
+
+git push "https://github.com/Solarisbank/identhub-android.git" $RELEASE_VERSION
